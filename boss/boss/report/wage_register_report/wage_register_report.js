e1627aa9261a0b6ac7ab1e595577a326d39fb26d
Abdulla P I <abdulla.pi@groupteampro.com>
2020-12-26 14:25:35 +0000

    First Commit Boss Customization

---
+++ boss/boss/report/wage_register_report/wage_register_report.js
@@ -0,0 +1,55 @@
+// Copyright (c) 2016, TeamPRO and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Wage Register Report"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_days(frappe.datetime.get_today(), -9),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_days(frappe.datetime.get_today(), 21),
+			"reqd": 1
+		},
+		{
+			"fieldname": "employee",
+			"label": __("Employee"),
+			"fieldtype": "Link",
+			"options": "Employee"
+		},
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname": "docstatus",
+			"label": __("Document Status"),
+			"fieldtype": "Select",
+			"options": ["Draft", "Submitted", "Cancelled"],
+			"default": "Draft"
+		},
+		{
+			"fieldname": "client",
+			"label": __("Client"),
+			"fieldtype": "Link",
+			"options": "Client"
+
+		},
+		{
+			"fieldname": "site",
+			"label": __("Site"),
+			"fieldtype": "Link",
+			"options": "Site",
+		}
+	]
+};
